448c997cf548fd8787502e077f87bc3a4de05d90
r.didukh <r.didukh@samsung.com>
2012-10-03 14:38:42 +0300

    First sound. Yahoo.

--- audio.cpp
+++ audio.cpp
@@ -9,13 +9,56 @@ using namespace std;
 
 static const double PI = 3.14159265;
 
+class WaveForm
+{
+	virtual double S(double x)=0;
+	public:
+	double operator()(double x)
+	{
+		return S(x);
+	}
+	
+	uint16_t operator()(unsigned int t, unsigned int rate, double max, int bits)
+	{
+		double t_i = (double)t/rate;
+		double s_i = (1<<(bits-1))*abs(S(t_i))/max;
+		return (uint16_t)s_i;
+	}
+
+	virtual ~WaveForm() {}
+
+};
+	
+class SinWaveForm: public WaveForm
+{
+	//double amp;
+	double freq;
+	//double phase;
+
+	virtual double S(double x)
+	{
+		return sin(2*PI*this->freq*x);
+	}
+	
+	public:
+	SinWaveForm(double freq)
+	{
+	//	this->amp = amp;
+		this->freq = freq;
+	//	this->phase = phase;
+	}
+
+};
+
 class WaveFileChunk
 {
 	protected:
 	std::string id;
 	uint32_t size;
 	public:
+		uint32_t getSize() { return this->size; }
 		virtual void serialize(std::ofstream& os)=0;
+		virtual ~WaveFileChunk() {}
 };
 
 class WaveFileHeader: public WaveFileChunk
@@ -33,13 +76,18 @@ public:
 	WaveFileHeader()
 	{
 		this->id = "RIFF";
-		this->size = 100; // IMPLEMENT SIZE COUNT
+		this->size = 0; // IMPLEMENT SIZE COUNT
 		this->type = "WAVE";
 	}
 
 	virtual void serialize(std::ofstream& os)
 	{
 		os << this->id; //<< this->size << this->type;
+
+		this->size = 8;
+		for(std::vector<WaveFileChunk*>::iterator i = this->chunks.begin(); i != this->chunks.end(); ++i)
+			this->size += (*i)->getSize() + 8;
+
 		os.write((char *)&(this->size), sizeof(this->size));	// IMPLEMENT SIZE COUNT	
 		os << this->type;
 
@@ -59,6 +107,12 @@ class WaveFileFormatChunk: public WaveFileChunk
 	uint16_t sample;
 //	uint16_t extra_size;
 public:
+	
+	uint16_t getCompression() { return this->compression; }
+	uint16_t getChannels() { return this->channels; }
+	uint32_t getRate() { return this->rate; }
+	uint32_t getSample() { return this->sample; }
+	
 	virtual void serialize(std::ofstream& os)
 	{
 		os << this->id;
@@ -71,15 +125,15 @@ public:
 		os.write((char *)&(this->sample), sizeof(this->sample));
 	}
 
-	WaveFileFormatChunk(uint16_t compression, uint16_t channels, uint32_t rate, uint32_t bpsec, uint16_t align, uint16_t sample)
+	WaveFileFormatChunk(uint16_t compression, uint16_t channels, uint32_t rate, uint16_t sample)
 	{
 		this->id = "fmt ";
 		this->size = 16;
 		this->compression = compression;
 		this->channels = channels;
 		this->rate = rate;
-		this->bpsec = bpsec;
-		this->align = align;
+		this->bpsec = channels*rate*sample/8;
+		this->align = 2;
 		this->sample = sample;
 	}
 
@@ -88,11 +142,32 @@ public:
 
 class WaveFileDataChunk: public WaveFileChunk
 {
+	unsigned int length;
+	WaveFileFormatChunk* fmt;
+	std::vector<WaveForm*> channels;	
+
 	public:
-	WaveFileDataChunk()
+	WaveFileDataChunk(WaveFileFormatChunk* fmt, unsigned int length):
+	channels(fmt->getChannels(), NULL)
 	{
 		this->id = "data";
-		this->size = 2049;	// IMPLEMENT SIZE COUNT
+		this->fmt = fmt;
+		this->length = length;
+		this->size = fmt->getChannels()*fmt->getRate()*fmt->getSample()/8;
+	}
+	
+	void setLength(unsigned int length)
+	{
+		this->length = length;
+		this->size = this->fmt->getChannels()*this->fmt->getRate()*this->fmt->getSample()/8;
+	}
+
+	void setChannelWaveForm(unsigned int ch, WaveForm* wf)
+	{
+		if(ch >= fmt->getChannels()) throw 1;
+	//	std::cout << "5" << std::endl;
+		this->channels[ch] = wf;
+	//	std::cout << "6" << std::endl;
 	}
 	
 	virtual void serialize(std::ofstream& os)
@@ -100,47 +175,20 @@ class WaveFileDataChunk: public WaveFileChunk
 		os << this->id;
 		os.write((char *)&(this->size), sizeof(this->size));
 		
-		/* IMPLEMENT DATA SERIALIZATION!!! */
+		for(unsigned int i = 1; i <= this->length; i++)
+		{
+			for(unsigned int j = 0; j < fmt->getChannels(); j++)
+			{
+		//		std::cout << "1" << std::endl;
+				WaveForm* wf = this->channels[j];
+		//		std::cout << "2" << std::endl;
+				uint16_t q = (*wf)(i, this->fmt->getRate(), 1.0, 16);
+		//	std::cout << "3" << std::endl;
+				os.write((char *)&q, sizeof(q));
+		//		std::cout << "4" << std::endl;
+			}
+		}
 
-	}
-
-};
-
-class WaveForm
-{
-	virtual double S(double x);
-	public:
-	double operator()(double x)
-	{
-		return S(x);
-	}
-//	WaveForm operator+(const WaveForm& wf);
-};
-
-class SinWaveForm
-{
-	//double amp;
-	double freq;
-	//double phase;
-
-	virtual double S(double x)
-	{
-		return sin(2*PI*x);
-	}
-	
-	uint16_t modulate(unsigned int t, double max, int bits)
-	{
-		double t_i = (double)t/freq;
-		double s_i = (1<<(bits-1))*abs(S(t_i))/max;
-		return (uint16_t)s_i;
-	}
-
-	public:
-	SinWaveForm(double freq)
-	{
-	//	this->amp = amp;
-		this->freq = freq;
-	//	this->phase = phase;
 	}
 
 };
@@ -180,18 +228,28 @@ int main(int argc, char** argv)
 	}
 
 	WaveFileHeader wfh;
-//WaveFileFormatChunk(uint16_t compression, uint16_t channels, uint32_t rate, uint32_t bpsec, uint16_t align, uint16_t sample)
 
-	WaveFileFormatChunk fmt(1, 1, 44100, 0x123456, 4, 16);
-	WaveFileDataChunk data;	
+	WaveFileFormatChunk *fmt = new WaveFileFormatChunk(1, 1, 44100, 16);
+	WaveFileDataChunk *data = new WaveFileDataChunk(fmt, 44100);	
+	
+	wfh.add(fmt);
+	wfh.add(data);
 
-	wfh.add(&fmt);
-	wfh.add(&data);
+	SinWaveForm * swf = new SinWaveForm(440);
+
+//	std::cout << "???" << std::endl;
+	
+	data->setChannelWaveForm(0, swf);
+	//data->setLength();
 
 	wfh.serialize(outfile);
 
 	outfile.close();
 
+	delete fmt;
+	delete data;
+	delete swf;
+	
 	return 0;
 }
 
